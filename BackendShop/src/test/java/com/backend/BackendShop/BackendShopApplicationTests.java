4af5063c06cea330542b8dd79a8cc1d908418aa1
Gonçalo Fonte <goncalofonte82@gmail.com>
2024-10-29 11:48:31 +0000

    first commit
    Adding a spring boot project

---
+++ BackendShop/src/test/java/com/backend/BackendShop/BackendShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.BackendShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
